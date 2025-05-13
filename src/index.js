e5a34be2fb7257df5f151074e5b23d7a54f2c1ff
SekgobelaME <sekgobelatumi010426@gmail.com>
2025-05-13 04:01:24 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+//import './override.css';
+//import './customStyles.css'; //custom styles
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
